db215735c37fd05c372c7e829d8e5dfdb6f95105
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-05-27 21:09:41 +0200

    initial commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "GrowStack Ai | Login",
+};
+
+export default function Login() {
+  return <div>Login</div>;
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "GrowStack Ai | Register",
+};
+
+export default function Register() {
+  return <div>Register</div>;
+}
