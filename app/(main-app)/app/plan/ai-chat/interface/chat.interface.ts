2e4492b945399d4ef0b36b66a1af27d6a38dfdc0
augustinebest <augustusbest00@gmail.com>
2024-07-15 00:32:20 +0100

    Exported ISidebarItem interface

---
+++ app/(main-app)/app/plan/ai-chat/interface/chat.interface.ts
@@ -0,0 +1,9 @@
+export interface ISidebarItem {
+  _id: string;
+  title: string;
+  createdDate: string;
+  updatedDate?: string;
+  selected: boolean;
+  onRename: (_id: string, newTitle: string) => void;
+  onSelect: () => void;
+}
