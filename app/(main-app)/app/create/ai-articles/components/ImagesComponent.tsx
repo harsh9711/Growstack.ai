9ba5a78ac3f8d18b8bc8175cba7fa1c966651617
augustinebest <augustusbest00@gmail.com>
2024-07-20 13:30:46 +0100

    Fixed Article Generation Format

--- app/(main-app)/app/create/ai-articles/components/ImagesComponent.tsx
+++ app/(main-app)/app/create/ai-articles/components/ImagesComponent.tsx
@@ -8,7 +8,12 @@ import clsx from "clsx";
 import { AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
-import { creativityOptions, languageOptions, povOptions, writingToneOptions } from "../constants/options";
+import {
+  creativityOptions,
+  languageOptions,
+  povOptions,
+  writingToneOptions,
+} from "../constants/options";
 import { ISubtitleTalkingPoints } from "../types";
 import AdvancedOptions from "./AdvancedOptions";
 import ImageDialog from "./dialogs/ImageDialog";
@@ -23,7 +28,9 @@ interface ImagesComponentProps {
   articleData: string;
   setArticleData: React.Dispatch<React.SetStateAction<string>>;
   images: Array<{ revised_prompt: string; url: string }>;
-  setImages: React.Dispatch<React.SetStateAction<Array<{ revised_prompt: string; url: string }>>>;
+  setImages: React.Dispatch<
+    React.SetStateAction<Array<{ revised_prompt: string; url: string }>>
+  >;
 }
 
 const ImagesComponent: React.FC<ImagesComponentProps> = ({
@@ -43,8 +50,12 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
   const [description, setDescription] = useState<string>("");
   const [showAdvanced, setShowAdvanced] = useState<boolean>(false);
   const [aiModel, setAiModel] = useState<string>("gpt-4o");
-  const [creativity, setCreativity] = useState<string>(creativityOptions[0].value);
-  const [writingTone, setWritingTone] = useState<string>(writingToneOptions[0].value);
+  const [creativity, setCreativity] = useState<string>(
+    creativityOptions[0].value
+  );
+  const [writingTone, setWritingTone] = useState<string>(
+    writingToneOptions[0].value
+  );
   const [language, setLanguage] = useState<string>(languageOptions[0].value);
   const [pov, setPov] = useState<string>(povOptions[0].value);
 
@@ -95,21 +106,8 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
         const {
           data: { data },
         } = response;
-        const articleId = data;
-        const eventSource = new EventSource(`${API_URL}/ai/api/v1/wizard/generate/stream/${articleId}`);
-        eventSource.onerror = (event) => {
-          eventSource.close();
-        };
         setCurrentStep(4);
-        eventSource.onmessage = (event) => {
-          const data: string = event.data;
-          console.log(data);
-          setArticleData((prevData) => {
-            const newData = prevData + data;
-            console.log(newData);
-            return newData;
-          });
-        };
+        setArticleData(data);
       })
       .catch((err) => {
         toast.error(err.response.data.message || err.message);
@@ -125,7 +123,10 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
   };
 
   return (
-    <Motion transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}>
+    <Motion
+      transition={{ duration: 0.5 }}
+      variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+    >
       <div className="flex gap-5 items-start !mt-10">
         <div className="w-full !bg-white shadow-box p-6 border rounded-3xl max-w-xl space-y-6">
           <div className="space-y-1.5">
@@ -133,7 +134,9 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
               <label className="font-medium" htmlFor="title">
                 Title <span className="text-rose-600">*</span>
               </label>
-              <span className="text-primary-black text-opacity-50 text-sm">0/2000</span>
+              <span className="text-primary-black text-opacity-50 text-sm">
+                0/2000
+              </span>
             </div>
             <input
               type="text"
@@ -154,12 +157,16 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               placeholder="Describe what your image is about"
-              className="h-[128px] w-full bg-[#F5F5F5] rounded-xl block resize-none p-4 text-[15px]"></textarea>
+              className="h-[128px] w-full bg-[#F5F5F5] rounded-xl block resize-none p-4 text-[15px]"
+            ></textarea>
           </div>
 
           <div className="flex gap-4 items-center !mt-8">
             <div className="bg-gray-200 w-full h-0.5" />
-            <button onClick={toggleAdvancedOptions} className="w-full text-primary-green text-base">
+            <button
+              onClick={toggleAdvancedOptions}
+              className="w-full text-primary-green text-base"
+            >
               {showAdvanced ? "Advanced options -" : "Advanced options +"}
             </button>
             <div className="bg-gray-200 w-full h-0.5" />
@@ -188,35 +195,56 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
               className={clsx(
                 "w-full p-2 h-12 !mt-4 text-white bg-primary-green rounded-xl hover:bg-opacity-90 flex justify-center items-center",
                 isPending && "opacity-70 cursor-not-allowed"
-              )}>
+              )}
+            >
               {isPending ? <Spinner /> : "Generate Image"}
             </button>
             <button
               onClick={generateArticle}
-              disabled={(isArticlePending && images.length < 1) || images.length >= 1}
+              disabled={
+                (isArticlePending && images.length < 1) || images.length >= 1
+              }
               className={clsx(
                 "w-full p-2 h-12 !mt-4 text-white bg-primary-green rounded-xl hover:bg-opacity-90 flex justify-center items-center",
-                isArticlePending && images.length < 1 && "opacity-70 cursor-not-allowed",
+                isArticlePending &&
+                  images.length < 1 &&
+                  "opacity-70 cursor-not-allowed",
                 images.length >= 1 && "cursor-not-allowed"
-              )}>
-              {isArticlePending && images.length < 1 ? <Spinner /> : "Skip this step"}
+              )}
+            >
+              {isArticlePending && images.length < 1 ? (
+                <Spinner />
+              ) : (
+                "Skip this step"
+              )}
             </button>
           </div>
         </div>
         <div className="w-full">
           <div className="bg-primary-green rounded-2xl py-5 px-7 flex items-center gap-4">
-            <span className="bg-white h-12 w-12 grid place-content-center rounded-full">{currentStep + 1}</span>
+            <span className="bg-white h-12 w-12 grid place-content-center rounded-full">
+              {currentStep + 1}
+            </span>
             <h2 className="text-lg font-semibold text-white">Images List</h2>
           </div>
-          {isPending && images.length < 1 && <div className="h-40 grid place-content-center">Please hang on as we generate the image for you...</div>}
+          {isPending && images.length < 1 && (
+            <div className="h-40 grid place-content-center">
+              Please hang on as we generate the image for you...
+            </div>
+          )}
           {images.length > 0 && (
             <div>
               <ul className="mt-5">
                 {images.map((image, index) => (
-                  <li key={index} className="p-10 mb-3 border rounded-xl bg-white text-lg flex flex-col items-center gap-5">
+                  <li
+                    key={index}
+                    className="p-10 mb-3 border rounded-xl bg-white text-lg flex flex-col items-center gap-5"
+                  >
                     <div className="space-y-1">
                       <h2 className="font-semibold">Revised prompt</h2>
-                      <p className="text-[16px] leading-relaxed">{image.revised_prompt}</p>
+                      <p className="text-[16px] leading-relaxed">
+                        {image.revised_prompt}
+                      </p>
                     </div>
                     <ImageDialog imageSrc={image.url} />
                   </li>
@@ -229,7 +257,8 @@ const ImagesComponent: React.FC<ImagesComponentProps> = ({
                   className={clsx(
                     "w-full p-2 h-14 mt-4 text-white sheen bg-primary-green rounded-xl max-w-[200px] flex gap-2 items-center justify-center",
                     isArticlePending && "opacity-70"
-                  )}>
+                  )}
+                >
                   {isArticlePending ? <Spinner /> : "Generate Article"}
                 </button>
               </div>
