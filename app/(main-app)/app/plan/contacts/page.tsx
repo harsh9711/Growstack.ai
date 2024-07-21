fa239e3993f5ffe2fbac769d78c67752c60d47bc
augustinebest <augustusbest00@gmail.com>
2024-07-21 17:19:03 +0100

    Removed Prospect

--- app/(main-app)/app/plan/contacts/page.tsx
+++ app/(main-app)/app/plan/contacts/page.tsx
@@ -55,10 +55,10 @@ export default function ContactsDashboard() {
         return (
           <Fragment>
             <h1 className="text-2xl font-semibold">Prospects</h1>
-            <p className="flex items-center gap-2 text-[#3D3D3D] text-opacity-50 text-[15px]">
+            {/* <p className="flex items-center gap-2 text-[#3D3D3D] text-opacity-50 text-[15px]">
               Lorem ipsum dolor sit amet consectetur. Vitae leo amet aliquam
               ultricies accumsan. Nec.{" "}
-            </p>
+            </p> */}
           </Fragment>
         );
     }
@@ -70,7 +70,7 @@ export default function ContactsDashboard() {
           <div className="space-y-2 w-full">
             {renderTitle(selectedTabIndex)}
           </div>
-          <div className="w-full flex items-center justify-end gap-3">
+          {/* <div className="w-full flex items-center justify-end gap-3">
             <div className="w-full max-w-[620px] bg-white shadow-2xl shadow-gray-200 px-3 py-2 rounded-xl">
               <div className="w-full flex relative">
                 {tabs.map((tab, index) => (
@@ -100,13 +100,13 @@ export default function ContactsDashboard() {
                   }}
                 ></div>
               </div>
-            </div>
+            </div> */}
             {/* <Link href="/app/plan/contacts/settings">
               <button className="border Fborder-[#EBEBEB] rounded-lg p-3 hover:bg-primary-light-gray text-primary-black">
                 <SettingsIcon size={20} />
               </button>
             </Link> */}
-          </div>
+          {/* </div> */}
         </div>
 
         <div className="mt-5">{renderContent(selectedTabIndex)}</div>
