bc9c1405ab43718da89fb76650e6824ba4e8b4f7
augustinebest <augustusbest00@gmail.com>
2024-07-24 09:43:35 +0100

    Created Microphone component

---
+++ app/(main-app)/app/plan/ai-chat/components/Microphone.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MicrophoneIcon, StopIcon } from "@/components/svgs";
+import useSpeechRecognition from "../../hooks/UseSpeechRecognition";
+import "../../../../../../styles/waveform.css";
+
+interface MicrophoneProps {
+  open: boolean;
+  isAnimating: boolean;
+  handleOpenChange: (status: boolean) => void;
+}
+
+const NUM_WAVES = 40; // Number of wave elements
+
+export default function Microphone({
+  open,
+  handleOpenChange,
+  isAnimating,
+}: MicrophoneProps) {
+  return (
+    <DropdownMenu open={open} onOpenChange={handleOpenChange}>
+      <DropdownMenuTrigger asChild>
+        <button
+          type="button"
+          onClick={() => handleOpenChange(!open)}
+          className="h-12 w-12 flex justify-center items-center bg-primary-green hover:bg-opacity-90 transition-all duration-300 text-white rounded-xl"
+        >
+          {isAnimating ? <StopIcon /> : <MicrophoneIcon />}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="!shadow-2xl !shadow-gray-900/20 mr-20 p-4 min-w-[20rem]">
+        <div className="waveform">
+          {Array.from({ length: NUM_WAVES }).map((_, index) => (
+            <div
+              key={index}
+              className={`wave ${!isAnimating ? "stop-animation" : ""}`}
+              style={{ animationDelay: `${index * 0.1}s` }}
+            ></div>
+          ))}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
