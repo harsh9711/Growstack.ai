d846a877300eb9274541f943c0eee543e570b3cb
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-07-27 04:25:08 -0700

    socialmedia fixing

--- app/(main-app)/app/engage/Social-media/components/Layout.tsx
+++ app/(main-app)/app/engage/Social-media/components/Layout.tsx
@@ -355,140 +355,8 @@ const Layout = () => {
   };
 
   return (
-    <div className="flex-1 max-h-[900px] flex  mt-10 shadow-lg rounded-3xl text-ellipsis">
-      {showRightSidebar && (
-        <aside className="w-full max-w-[350px] relative border bg-white rounded-l-3xl flex flex-col transition-all duration-300">
-          <div className="flex flex-row gap-6  py-4 px-6">
-            <Select value={selectedOption} onValueChange={setSelectedOption}>
-              <SelectTrigger className="w-[300px] h-12  text-black bg-gray-100 border-0 rounded-xl flex items-center justify-between px-4">
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    x="2.66675"
-                    y="2.66406"
-                    width="4"
-                    height="4"
-                    rx="0.666667"
-                    stroke="#034737"
-                    stroke-width="1.16667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <rect
-                    x="9.33325"
-                    y="2.66406"
-                    width="4"
-                    height="4"
-                    rx="0.666667"
-                    stroke="#034737"
-                    stroke-width="1.16667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <rect
-                    x="2.66675"
-                    y="9.33594"
-                    width="4"
-                    height="4"
-                    rx="0.666667"
-                    stroke="#034737"
-                    stroke-width="1.16667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M9.33325 10.7448C9.01109 10.7448 8.74992 11.006 8.74992 11.3281C8.74992 11.6503 9.01109 11.9115 9.33325 11.9115V10.7448ZM13.3333 11.9115C13.6554 11.9115 13.9166 11.6503 13.9166 11.3281C13.9166 11.006 13.6554 10.7448 13.3333 10.7448V11.9115ZM11.9166 9.32812C11.9166 9.00596 11.6554 8.74479 11.3333 8.74479C11.0111 8.74479 10.7499 9.00596 10.7499 9.32812H11.9166ZM10.7499 13.3281C10.7499 13.6503 11.0111 13.9115 11.3333 13.9115C11.6554 13.9115 11.9166 13.6503 11.9166 13.3281H10.7499ZM9.33325 11.9115H13.3333V10.7448H9.33325V11.9115ZM10.7499 9.32812V13.3281H11.9166V9.32812H10.7499Z"
-                    fill="#034737"
-                  />
-                </svg>
-                <SelectValue placeholder="Select an option">
-                  {selectedOptionLabel && (
-                    <div className="flex items-center gap-2">
-                      <span className="min-w-fit">
-                        {
-                          options.find(
-                            (option) => option.value === selectedOption
-                          )?.icon
-                        }
-                      </span>
-                      {selectedOptionLabel}
-                    </div>
-                  )}
-                </SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  {options.map(({ icon, label, value }) => (
-                    <SelectItem key={value} value={value}>
-                      <div
-                        className={clsx(
-                          "flex items-center gap-2",
-                          selectedOption === value && "text-black font-medium"
-                        )}
-                      >
-                        <span className="min-w-fit">{icon}</span>
-                        {label}
-                      </div>
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>{" "}
-            <button
-              className="transition-all duration-300"
-              onClick={toggleMenu}
-            >
-              <svg
-                width="42"
-                height="22"
-                viewBox="0 0 22 22"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="22" height="22" rx="5" fill="#034737" />
-                <path
-                  d="M6.64645 10.6464C6.45118 10.8417 6.45118 11.1583 6.64645 11.3536L9.82843 14.5355C10.0237 14.7308 10.3403 14.7308 10.5355 14.5355C10.7308 14.3403 10.7308 14.0237 10.5355 13.8284L7.70711 11L10.5355 8.17157C10.7308 7.97631 10.7308 7.65973 10.5355 7.46447C10.3403 7.2692 10.0237 7.2692 9.82843 7.46447L6.64645 10.6464ZM15 10.5L7 10.5L7 11.5L15 11.5L15 10.5Z"
-                  fill="white"
-                />
-              </svg>
-            </button>{" "}
-          </div>
-          <div className="relative p-5 flex-1  ">
-            <SidebarAccount
-              title={"GrowStackAI"}
-              author={"Twitter profile"}
-              imageUrl={"/facebook.png"}
-            />
-            <SidebarAccount
-              title={"GrowStackAI"}
-              author={"Twitter profile"}
-              imageUrl={"/twitter.png"}
-            />
-            <SidebarAccount
-              title={"GrowStackAI"}
-              author={"Twitter profile"}
-              imageUrl={"/insta.png"}
-            />
-            <SidebarAccount
-              title={"GrowStackAI"}
-              author={"Twitter profile"}
-              imageUrl={"/facebook.png"}
-            />
-          </div>
-          <div className="items-center justify-center  translate-x-24 -translate-y-14 ">
-            <button className="text-white bg-primary-green hover:bg-primary-green/90 w-44 justify-center flex gap-2 items-center h-12 font-medium rounded-xl transition-all duration-300 text-sm">
-              <BsPlus className="text-2xl" />
-              Add Account
-            </button>
-          </div>
-          {/* <div className="h-20 w-full bg-gradient-to-b from-transparent via-white to-white absolute bottom-0 " /> */}
-        </aside>
-      )}
+    <div className="flex-1 max-h-[780px] flex  mt-10 shadow-lg rounded-3xl text-ellipsis">
+   
       <aside
         className={clsx(
           "w-full max-w-[350px] relative border bg-white   flex flex-col",
@@ -499,35 +367,27 @@ const Layout = () => {
           <div className="relative max-w-40 flex flex-row gap-8 font-normal text-ellipsis">
             <button
               onClick={() => handleButtonClick(0)}
-              className={`transition-all text-[16px] duration-300 ${activeIndex === 0 ? "text-green-800 font-semibold" : ""
+              className={`transition-all text-[14px] duration-300 ${activeIndex === 0 ? "text-green-800 font-semibold" : ""
                 }`}
             >
-              All
+            Message
             </button>
             <button
               onClick={() => handleButtonClick(1)}
-              className={`transition-all text-[16px] duration-300 ${activeIndex === 1 ? "text-green-800 font-semibold" : ""
+              className={`transition-all text-[14px] duration-300 ${activeIndex === 1 ? "text-green-800 font-semibold" : ""
                 }`}
             >
-              My
+            Comments
             </button>
-            <button
-              onClick={() => handleButtonClick(2)}
-              className={`transition-all duration-300 text-[16px] ${activeIndex === 2 ? "text-green-800 font-semibold" : ""
-                }`}
-            >
-              Unassigned
-            </button>
+        
             <div
-              className={`absolute -bottom-3 h-1 bg-green-800 transition-all duration-300  ${activeIndex === 2
-                  ? "-left-[110px]"
-                  : activeIndex === 1
-                    ? "-left-0"
+              className={`absolute -bottom-3 h-1 bg-green-800 transition-all duration-300  ${activeIndex === 1
+                    ? "-left-2.5"
                     : "-left-2"
                 }`}
               style={{
                 transform: `translateX(${activeIndex * 100}%)`,
-                width: activeIndex === 2 ? "65%" : "25%",
+                width: activeIndex === 1 ? "60%" : "55%",
               }}
             />
           </div>
@@ -538,54 +398,7 @@ const Layout = () => {
                 className="shadow-lg transition-all duration-300"
                 onClick={toggleMenu}
               >
-                <svg
-                  className={clsx(
-                    "w-6 h-6 transition-transform",
-                    MenuRotated ? "rotate-180" : ""
-                  )}
-                  viewBox="0 0 22 22"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect width="22" height="22" rx="5" fill="#034737" />
-                  <rect
-                    x="5.16797"
-                    y="5.66406"
-                    width="4"
-                    height="4"
-                    rx="0.666667"
-                    stroke="white"
-                    stroke-width="1.16667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <rect
-                    x="11.832"
-                    y="5.66406"
-                    width="4"
-                    height="4"
-                    rx="0.666667"
-                    stroke="white"
-                    stroke-width="1.16667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <rect
-                    x="5.16797"
-                    y="12.3359"
-                    width="4"
-                    height="4"
-                    rx="0.666667"
-                    stroke="white"
-                    stroke-width="1.16667"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M11.832 13.7448C11.5099 13.7448 11.2487 14.006 11.2487 14.3281C11.2487 14.6503 11.5099 14.9115 11.832 14.9115V13.7448ZM15.832 14.9115C16.1542 14.9115 16.4154 14.6503 16.4154 14.3281C16.4154 14.006 16.1542 13.7448 15.832 13.7448V14.9115ZM14.4154 12.3281C14.4154 12.006 14.1542 11.7448 13.832 11.7448C13.5099 11.7448 13.2487 12.006 13.2487 12.3281H14.4154ZM13.2487 16.3281C13.2487 16.6503 13.5099 16.9115 13.832 16.9115C14.1542 16.9115 14.4154 16.6503 14.4154 16.3281H13.2487ZM11.832 14.9115H15.832V13.7448H11.832V14.9115ZM13.2487 12.3281V16.3281H14.4154V12.3281H13.2487Z"
-                    fill="white"
-                  />
-                </svg>
+          
               </button>
               <button
                 className="shadow-lg transition-all duration-300"
@@ -916,7 +729,8 @@ const Layout = () => {
         </main>
 
       )}
-      {isOpened && (<aside
+      {isOpened && activeIndex === 1 ? (
+        <aside
         className={clsx(
           "w-full max-w-[350px] relative border bg-white rounded-r-3xl  flex flex-col",
         )}
@@ -1052,7 +866,7 @@ const Layout = () => {
       </div>
     </div>
         <div className="border-[0.5px] border-gray-200 my-4"></div>
-        <div className="flex flex-row items-center gap-4  px-6">
+        {/* <div className="flex flex-row items-center gap-4  px-6">
           <svg
             width="16"
             height="16"
@@ -1092,11 +906,11 @@ const Layout = () => {
             />
           </svg>
           <h2 className="text-[16px] font-medium"> Assignees</h2>
-        </div>
+        </div> */}
         <div className="relative">
-          <div className="px-6 py-2 cursor-pointer" onClick={toggleFilterMenu}>
+          {/* <div className="px-6 py-2 cursor-pointer" onClick={toggleFilterMenu}>
             <Image src="/circlec.png" alt="cicle" width={50} height={50} />
-          </div>
+          </div> */}
 
           {isFilterMenuOpen && (
             <div
@@ -1166,7 +980,7 @@ const Layout = () => {
             </div>
           )}
         </div>
-      </aside>)}
+      </aside>):null}
 
     </div>
   );
