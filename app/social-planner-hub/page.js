5450e4ebf781000191157cd67694c8dd2a6acfd7
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-08-09 04:26:58 +0200

    fix: resolve issues in QA review --5 [styles fix]

--- app/social-planner-hub/page.js
+++ app/social-planner-hub/page.js
@@ -1,27 +1,28 @@
-'use client';
-import Footer from '@/components/footer/Footer';
-import Faqs from '@/components/home/faq/Faqs';
-import Navbar from '@/components/navbar/Navbar';
-import Banner from '@/components/socialPlannerHub/banner/Banner';
-import Partnership from '@/components/socialPlannerHub/partnership/Partnership';
-import PlannerSolution from '@/components/socialPlannerHub/plannerSolution/PlannerSolution';
-import Schedule from '@/components/socialPlannerHub/schedule/Schedule';
-import SocialInteraction from '@/components/socialPlannerHub/socialInteraction/SocialInteraction';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+"use client";
+import Footer from "@/components/footer/Footer";
+import Faqs from "@/components/home/faq/Faqs";
+import Navbar from "@/components/navbar/Navbar";
+import Banner from "@/components/socialPlannerHub/banner/Banner";
+import Partnership from "@/components/socialPlannerHub/partnership/Partnership";
+import PlannerSolution from "@/components/socialPlannerHub/plannerSolution/PlannerSolution";
+import Schedule from "@/components/socialPlannerHub/schedule/Schedule";
+import SocialInteraction from "@/components/socialPlannerHub/socialInteraction/SocialInteraction";
+import React from "react";
+// I commented this bootstrapp CDN because of conflicting styles with tailwindcss, NB: I'll uncomment it as soon as reliable solution is found
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Page() {
   return (
     <React.Fragment>
       <div className="clrNav">
-      <Navbar logoUrl="/images/growhite.svg" logoAlt="Custom Logo" />
-      <Banner />
-       <PlannerSolution />
-       <Partnership />
-       <SocialInteraction />
-       <Schedule />
-       <Faqs />
-       <Footer />
+        <Navbar logoUrl="/images/growhite.svg" logoAlt="Custom Logo" />
+        <Banner />
+        <PlannerSolution />
+        <Partnership />
+        <SocialInteraction />
+        <Schedule />
+        <Faqs />
+        <Footer />
       </div>
     </React.Fragment>
   );
