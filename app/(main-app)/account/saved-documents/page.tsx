2b52bca189df544cd4f2da350e2a29a18bf2c24c
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-08-10 04:51:19 +0200

    fix: resolved QA issues

--- app/(main-app)/account/saved-documents/page.tsx
+++ app/(main-app)/account/saved-documents/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import React from "react";
 import DocumentsTable from "./DocumentsTable";
+import { ArrowBack } from "@/components/svgs";
+import { useRouter } from "next/router";
 
 export default function SavedDocuments() {
+  const router = useRouter();
+
   return (
     <div>
       <div className="mt-10">
-        <h1 className="text-2xl font-semibold">All documents</h1>
+        <div className="w-full flex justify-between items-center">
+          <h1 className="text-2xl font-semibold">All documents</h1>
+          <button
+            onClick={() => router.back()}
+            className="text-[#212833] hover:bg-primary-green/10 sheen flex gap-2 px-3.5 py-1.5 rounded-full font-medium items-center">
+            <ArrowBack />
+            Back
+          </button>
+        </div>
         <div className="bg-white border rounded-3xl mt-5">
           <DocumentsTable />
         </div>
