2111ba0add2aee0955cbfeae98a2c09b4035e0d3
umarthirtynorth <103989082+umarthirtynorth@users.noreply.github.com>
2024-08-10 18:01:35 +0500

    Added download functionality in documents table

--- app/(main-app)/account/saved-documents/DocumentsTable.tsx
+++ app/(main-app)/account/saved-documents/DocumentsTable.tsx
@@ -3,10 +3,20 @@
 import Motion from "@/components/Motion";
 import Spinner from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import instance from "@/config/axios.config";
 import { API_URL } from "@/lib/api";
-import { editDocument, savedDecument } from "@/lib/features/documents/document.slice";
+import {
+  editDocument,
+  savedDecument,
+} from "@/lib/features/documents/document.slice";
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -30,6 +40,7 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import swal from "sweetalert";
+import { downloadTxt } from "../../app/plan/custom-gpts/new/components/utils/downloadHelpers";
 
 interface Document {
   name: string;
@@ -166,11 +177,38 @@ export default function DocumentsTable() {
   const [documents, setDocuments] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const downloadTxtFile = (rowData: any) => {
+    const textData = `
+      ID: ${rowData._id}
+      User ID: ${rowData.user_id}
+      Document Type: ${rowData.doc_type}
+      Document Name: ${rowData.doc_name}
+      Language: ${rowData.doc_language}
+      Word Count: ${rowData.doc_words}
+      Category: ${rowData.category}
+      Content: ${rowData.doc_content}
+      Created At: ${rowData.createdAt}
+      Updated At: ${rowData.updatedAt}
+    `;
+
+    const blob = new Blob([textData], { type: "text/plain" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${rowData.doc_name}.txt`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  };
+
   const columns: ColumnDef<Document>[] = [
     {
       accessorKey: "doc_name",
       header: () => <div className="uppercase">Document Name</div>,
-      cell: ({ row }) => <div className="capitalize">{row.getValue("doc_name")}</div>,
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("doc_name")}</div>
+      ),
     },
     // {
     //   accessorKey: "workbook",
@@ -182,13 +220,25 @@ export default function DocumentsTable() {
     {
       accessorKey: "category",
       header: () => <div className="uppercase">Category</div>,
-      cell: ({ row }) => <div className={clsx("uppercase bg-gray-100 px-2 py-1.5 max-w-fit rounded-md text-[13px]")}>{row.getValue("category")}</div>,
+      cell: ({ row }) => (
+        <div
+          className={clsx(
+            "uppercase bg-gray-100 px-2 py-1.5 max-w-fit rounded-md text-[13px]"
+          )}
+        >
+          {row.getValue("category")}
+        </div>
+      ),
     },
     {
       accessorKey: "createdAt",
       header: () => <div className="uppercase">Created</div>,
       cell: ({ row }: any) => (
-        <div className="capitalize">{row.original.createdAt ? moment(row.original.createdAt).format("MMMM DD YYYY • hh:mm A") : "—"}</div>
+        <div className="capitalize">
+          {row.original.createdAt
+            ? moment(row.original.createdAt).format("MMMM DD YYYY • hh:mm A")
+            : "—"}
+        </div>
       ),
     },
     {
@@ -198,7 +248,16 @@ export default function DocumentsTable() {
         row.original.language ? (
           <div className="flex items-center gap-2.5">
             {row.original.doc_language && (
-              <Image src={languageFlags[row.getValue("doc_language") as keyof typeof languageFlags]} alt="" width={20} height={20} />
+              <Image
+                src={
+                  languageFlags[
+                    row.getValue("doc_language") as keyof typeof languageFlags
+                  ]
+                }
+                alt=""
+                width={20}
+                height={20}
+              />
             )}
             {row.getValue("doc_language")}
           </div>
@@ -209,7 +268,12 @@ export default function DocumentsTable() {
     {
       accessorKey: "doc_words",
       header: () => <div className="uppercase">Words Used</div>,
-      cell: ({ row }) => (row.getValue("doc_words") ? <div>{row.getValue("doc_words")}</div> : <div>—</div>),
+      cell: ({ row }) =>
+        row.getValue("doc_words") ? (
+          <div>{row.getValue("doc_words")}</div>
+        ) : (
+          <div>—</div>
+        ),
     },
     {
       id: "actions",
@@ -220,8 +284,18 @@ export default function DocumentsTable() {
             {/* <button className="p-1.5 hover:bg-gray-200 rounded-lg transition duration-300" onClick={() => handleEdit(row?.original)}>
               <Edit size={20} className="text-gray-800 cursor-pointer" />
             </button> */}
-            <button className="p-1.5 hover:bg-gray-200 rounded-lg transition duration-300" onClick={() => handleDeleteDocs(row?.original?._id)}>
+            <button
+              className="p-1.5 hover:bg-gray-200 rounded-lg transition duration-300"
+              onClick={() => handleDeleteDocs(row?.original?._id)}
+            >
               <MdDelete size={20} className="text-gray-800 cursor-pointer" />
+            </button>
+            <button className="p-1.5 hover:bg-gray-200 rounded-lg transition duration-300">
+              <FileDown
+                size={20}
+                className="text-gray-800 cursor-pointer"
+                onClick={() => downloadTxtFile(row?.original)}
+              />
             </button>
           </div>
         );
@@ -266,8 +340,11 @@ export default function DocumentsTable() {
         onClick={() => table.setPageIndex(i)}
         className={clsx(
           "w-12 h-[45px] rounded-lg mx-1 bg-[#4B465C14] transition-all duration-300",
-          i === table.getState().pagination.pageIndex ? "!bg-primary-green hover:bg-opacity-50 text-white" : "hover:bg-[#4B465C29]"
-        )}>
+          i === table.getState().pagination.pageIndex
+            ? "!bg-primary-green hover:bg-opacity-50 text-white"
+            : "hover:bg-[#4B465C29]"
+        )}
+      >
         {i + 1}
       </button>
     );
@@ -275,7 +352,9 @@ export default function DocumentsTable() {
 
   const handleGetDocumentsData = async () => {
     try {
-      const response = await instance.get(`${API_URL}/users/api/v1/docs?page=${pagination.pageIndex + 1}&limit=10`);
+      const response = await instance.get(
+        `${API_URL}/users/api/v1/docs?page=${pagination.pageIndex + 1}&limit=10`
+      );
       setDocuments(response?.data?.data?.docs);
     } catch (error) {
       console.log("Error fetching Documents:", error);
@@ -295,7 +374,9 @@ export default function DocumentsTable() {
     }).then(async (willDelete) => {
       if (willDelete) {
         try {
-          const response = await instance.delete(`${API_URL}/users/api/v1/docs/${id}`);
+          const response = await instance.delete(
+            `${API_URL}/users/api/v1/docs/${id}`
+          );
           if (response.data.success) {
             handleGetDocumentsData();
           }
@@ -324,13 +405,20 @@ export default function DocumentsTable() {
   };
 
   return (
-    <Motion transition={{ duration: 0.2 }} variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}>
+    <Motion
+      transition={{ duration: 0.2 }}
+      variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+    >
       <div className="w-full">
         <div className="flex items-center justify-between px-5 pt-5">
           <h2 className="text-lg font-semibold">All my documents</h2>
           <div className="bg-white border border-[#EBEBEB] px-4 py-1 rounded-xl flex gap-3 items-center w-full max-w-md">
             <Search className="text-[#5A5963]" />
-            <input type="search" className="outline-none h-[40px] w-full" placeholder="Search" />
+            <input
+              type="search"
+              className="outline-none h-[40px] w-full"
+              placeholder="Search"
+            />
           </div>
         </div>
         {loading ? (
@@ -347,7 +435,14 @@ export default function DocumentsTable() {
                     <TableRow key={headerGroup.id} className="bg-[#0347370D]">
                       {headerGroup.headers.map((header) => {
                         return (
-                          <TableHead key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+                          <TableHead key={header.id}>
+                            {header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                                )}
+                          </TableHead>
                         );
                       })}
                     </TableRow>
@@ -356,15 +451,27 @@ export default function DocumentsTable() {
                 <TableBody>
                   {table.getRowModel().rows?.length ? (
                     table.getRowModel().rows.map((row) => (
-                      <TableRow key={row.id} data-state={row.getIsSelected() && "selected"} className="bg-white">
+                      <TableRow
+                        key={row.id}
+                        data-state={row.getIsSelected() && "selected"}
+                        className="bg-white"
+                      >
                         {row.getVisibleCells().map((cell) => (
-                          <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                          <TableCell key={cell.id}>
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </TableCell>
                         ))}
                       </TableRow>
                     ))
                   ) : (
                     <TableRow className="hover:bg-white">
-                      <TableCell colSpan={columns.length} className="h-[50vh] text-center font-semibold text-lg hover:bg-white">
+                      <TableCell
+                        colSpan={columns.length}
+                        className="h-[50vh] text-center font-semibold text-lg hover:bg-white"
+                      >
                         No results.
                       </TableCell>
                     </TableRow>
@@ -380,7 +487,8 @@ export default function DocumentsTable() {
                     size="sm"
                     className="bg-[#4B465C14] hover:bg-[#4B465C29] border-none h-[45px]"
                     onClick={() => table.previousPage()}
-                    disabled={!table.getCanPreviousPage()}>
+                    disabled={!table.getCanPreviousPage()}
+                  >
                     Previous
                   </Button>
                   <div>
@@ -391,7 +499,8 @@ export default function DocumentsTable() {
                     size="sm"
                     className="bg-[#4B465C14] hover:bg-[#4B465C29] border-none h-[45px] px-4"
                     onClick={() => table.nextPage()}
-                    disabled={!table.getCanNextPage()}>
+                    disabled={!table.getCanNextPage()}
+                  >
                     Next
                   </Button>
                 </div>
