672b95a417d30de4673d87b8a3189e449481d54f
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-08-23 04:18:03 -0700

    startup

---
+++ app/solutions/revops copy/components/GridBoxes2.tsx
@@ -0,0 +1,231 @@
+
+
+  "use client";
+import { ArrowRight, Circle } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import useInView from "../useInView";
+import "./grid.css"
+const GridComponentSecond = () => {
+  const [selected, setSelected] = useState<number | null>(null);
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+  const [isTimerActive, setIsTimerActive] = useState<boolean>(false);
+  const isInView = useInView({ threshold: 0.5 });
+  useEffect(() => {
+    if (isInView) {
+      if (!isTimerActive) {
+        setIsTimerActive(true);
+      }
+      if (selected === null) {
+        handleClick(1);
+      }
+    } else {  
+      if (isTimerActive) {
+        setIsTimerActive(false);
+        if (timer) {
+          clearTimeout(timer);
+        }
+      }
+    }
+  }, [isInView]);
+
+  useEffect(() => {
+    if (isTimerActive) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      const newTimer = setTimeout(() => {
+        setSelected((prev) => {
+          const nextValue = prev === 4 ? 1 : (prev ?? 0) + 1;
+          if (nextValue === 1 && prev === 4) {
+            setIsTimerActive(false);
+            return null;
+          }
+          return nextValue;
+        });
+      }, 4000);
+
+      setTimer(newTimer);
+
+      return () => {
+        if (newTimer) {
+          clearTimeout(newTimer);
+        }
+      };
+    }
+  }, [selected, isTimerActive]);
+
+  const handleClick = (index: number) => {
+    setSelected(index);
+    setIsTimerActive(true);
+    if (timer) {
+      clearTimeout(timer);
+    }
+  };  
+  const handleComponentClick = () => {
+    if (isTimerActive) {
+      setIsTimerActive(false);
+      if (timer) {
+        clearTimeout(timer);
+      }
+    }
+  };
+ 
+
+    const renderContent = () => {
+      switch (selected) {
+        case 1:
+          return (
+            <div className="flex flex-col gap-y-4  items-center justify-center w-full ">
+              
+            <div className="rounded-2xl flex justify-center items-center relative z-[20] opacity-100">
+              <Image src="/salesrevops2/boxsvgs/boxsvg1.svg" alt="Box 1" width={940} height={555} className="w-[1000px] bg-cover z-[20]" />
+            </div>
+            <div className="flex z-[80] flex-row gap-2 w-full max-w-[950px] justify-center relative">
+              <div onClick={() => handleClick(2)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+              Campaign tracking
+              </div>
+              <div onClick={() => handleClick(3)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+             Comprehensive
+              reports
+              </div>
+              <div onClick={() => handleClick(4)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+              Performance insights
+              </div>
+            </div>
+          </div>
+          );
+        case 2:
+          return (
+            <div className="flex flex-col gap-y-4 items-center justify-center w-full">
+          <div className="rounded-2xl flex justify-center items-center relative z-[40] opacity-100">
+              <Image src="/salesrevops2/boxsvgs/boxsvg2.svg" alt="Box 1" width={1240} height={755} className="w-[1000px]  bg-cover z-[20]" />
+              </div>
+              <div className="flex  z-[80] flex-row items-center left-0 justify-center gap-2 w-full  max-w-[950px]   relative">
+                <div onClick={() => handleClick(1)} className="bg-white border whitespace-nowrap shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+             Engagement metrics
+                </div>
+                <div onClick={() => handleClick(3)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+             Comprehensive
+              reports
+              </div>
+              <div onClick={() => handleClick(4)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+              Performance insights 
+              </div>
+              </div>
+            </div>
+          );
+        case 3:
+          return (
+            <div className="flex flex-col gap-y-4 items-center justify-center w-full">
+          <div className="rounded-2xl flex justify-center relative  z-[40] items-center">
+              <Image src="/salesrevops2/boxsvgs/boxsvg3.svg" alt="Box 1" width={1240} height={755} className="w-[1000px]  bg-cover z-[20]" />
+              </div>
+              <div className="flex z-[80] flex-row gap-2 w-full max-w-[950px] justify-center relative">
+             <div onClick={() => handleClick(1)} className="bg-white border whitespace-nowrap shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+             Engagement metrics
+
+                </div>
+                <div onClick={() => handleClick(2)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+              Campaign tracking
+              </div>
+                <div onClick={() => handleClick(4)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+                Performance insights 
+              </div>
+              </div>
+            </div>
+          );
+        case 4:
+          return (
+            <div className="flex flex-col gap-y-4 items-center justify-center w-full ">
+            <div className="rounded-2xl flex justify-center items-center relative z-[40]">
+              <Image src="/salesrevops2/boxsvgs/boxsvg4.svg" alt="Box 1" width={1240} height={755} className="w-[1000px]  bg-cover z-[20]" />
+            </div>
+            <div className="flex z-[80] flex-row gap-2 w-full  max-w-[950px] justify-center relative">
+           <div onClick={() => handleClick(1)} className="bg-white border whitespace-nowrap shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+           Engagement metrics
+                </div>
+                <div onClick={() => handleClick(2)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+              Campaign tracking
+              </div>
+              <div onClick={() => handleClick(3)} className="bg-white border shadow-md hover:shadow-lg max-w-[400px] w-full max-h-[82px] h-full text-center p-4 rounded-2xl">
+             Comprehensive
+              reports
+              </div>
+            </div>
+          </div>
+          
+          );
+      
+          default:
+          return (
+    <>      
+
+          <div className="xl:grid 2xl:grid grid-cols-3 relative 2xl:gap-x-16 xl:left-60 2xl:left-72 justify-center items-center">
+  <div
+    onClick={() => handleClick(1)}
+    className="rounded-[30px] flex justify-center items-center z-[60] cursor-pointer hover:scale-105 transition-transform duration-300"
+  >
+    <Image
+      src="/solutions/salesrevops2/boxes/box1.svg"
+      alt="Box 1"
+      width={400}
+      height={420}
+      className="w-[480px] h-[357px] hover-boxsalesrevops11"
+    />
+  </div>
+  <div
+    onClick={() => handleClick(2)}
+    className="rounded-[30px] flex justify-center items-center z-[60] cursor-pointer hover:scale-105  transition-transform duration-300"
+  >
+    <Image
+      src="/solutions/salesrevops2/boxes/box2.svg"
+      alt="Box 2"
+      width={400}
+      height={400}
+      className="w-[480px] h-[357px] hover-boxsalesrevops21"
+    />
+  </div>
+  <div className="rounded-[30px] relative right-[810px] 2xl:top-44 flex justify-center items-center hover:scale-150 transition-transform duration-300">
+    <Image
+      src="/solutions/salesrevops2/boxes/main.svg"
+      alt="Main"
+      width={50}
+      height={50}
+    />
+  </div>
+ 
+  <div
+    onClick={() => handleClick(4)}
+    className="rounded-[30px] flex relative top-2 justify-center items-center z-[60] cursor-pointer hover:scale-105 transition-transform duration-300"
+  >
+    <Image
+      src="/solutions/salesrevops2/boxes/box4.svg"
+      alt="Box 4"
+      width={380}
+      height={420}
+      className="w-[480px] h-[357px] hover-boxsalesrevops41"
+    />
+  </div> 
+  <div
+    onClick={() => handleClick(3)}
+    className="rounded-[30px] relative top-2 flex justify-center items-center z-[60] cursor-pointer hover:scale-105  transition-transform duration-300"
+  >
+    <Image
+      src="/solutions/salesrevops2/boxes/box3.svg"
+      alt="Box 3"
+      width={380}
+      height={400}
+      className="w-[480px] h-[357px] hover-boxsalesrevops31"
+    />
+  </div>
+</div>
+</> 
+          );
+      }
+    };
+    return <div className="flex flex-col justify-center 2xl:text-[14px] text-[8px] 2xl:p-0 p-4 relative 2xl:left-8 mx-auto items-center overflow-x-hidden" onClick={handleComponentClick}>  
+{renderContent()}</div>;
+  };
+  export default GridComponentSecond
---
+++ app/solutions/revops copy/page.tsx
@@ -0,0 +1,251 @@
+"use client";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import ImageGalleryLine from "./components/ImageGalleryline";
+import Navbar from "@/components/navbar/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import GridComponent from "./components/GridBoxes";
+import GridComponentSecond from "./components/GridBoxes2";
+import ImageGalleryLineResponsive from "./components/ImageGallerylineresponsive";
+import LoadingBar from "./components/Loading";
+import MarketingStreamline from "./components/marketingStreamline/MarketingStreamline";
+import Footer from "@/components/footer/Footer";
+import LoadingBarSecond from "./components/LoadingBar";
+import Banner from "./banner/Banner";
+
+const page = () => {
+  useEffect(() => {
+    AOS.init({ duration: 500 });
+  }, []);
+  return (
+    <main className="">
+      <section className="bg-[#E2F0CB] w-full mb-10 2xl:mb-20 items-center justify-center mx-auto">
+        <Navbar
+          logoUrl="/images/logo.png"
+          logoAlt="Custom Logo"
+          backgroundColor="transparent"
+        />
+
+        <div className="relative flex items-center max-w-[1220px] 2xl:max-w-[1350px] 2xl:p-0 p-4 w-full 2xl:max-h-[1112px] h-full justify-center mx-auto">
+          <div className="w-full flex flex-row justify-between brightness-110 relative gap-x-20  items-center mt-10 mb-10 2xl:mt-32 2xl:mb-40">
+            <div className="w-full max-w-[700px] gap-y-4 flex flex-col">
+              <div
+                data-aos="fade-right"
+                data-aos-duration="1200"
+                className="bg-white text-black py-2 2xl:px-4 text-center items-center justify-center flex text-[10px] 2xl:text-[12px] rounded-full font-semibold uppercase max-w-[300px] 2xl:max-w-[352px] shadow-lg w-full tracking-widest"
+              >
+                Growstack for Revenue operations Team
+              </div>
+
+              <div className="2xl:max-w-3xl w-full brightness-95">
+                <h1
+                  data-aos="fade-right"
+                  data-aos-duration="1500"
+                  className="text-[26px] xl:text-[48px]   w-full leading-normal text-black"
+                >
+                  <span className="font-semibold">Optimize your revenue </span>
+                  <br />{" "}
+                  <span className="font-light 2xl:whitespace-nowrap">
+                    strategy with Growstack
+                  </span>
+                </h1>
+                <p
+                  data-aos="fade-right"
+                  data-aos-duration="1500"
+                  className="text-[16px] 2xl:text-[18px] text-black mt-4 w-full max-w-[600px] leading-normal font-light"
+                >
+                  Take your revenue operations to the next level with
+                  Growstack’s cutting-edge tools and solutions. From optimizing
+                  data management to enhancing team alignment, our platform
+                  addresses your key challenges and drives your team towards
+                  achieving exceptional results.
+                </p>
+
+                <div className="flex flex-col gap-20 mt-10">
+                  <div
+                    data-aos="fade-right"
+                    data-aos-duration="1500"
+                    className="flex flex-row gap-8 group text-[12px] 2xl:text-[18px]"
+                  >
+                    <button className="bg-white font-medium flex items-center gap-2 text-[#034737] 2xl:py-4 2xl:px-7 rounded-xl py-2 px-2 group-hover:font-bold shadow-md hover:shadow-">
+                      Get free trial <ArrowRight />
+                    </button>
+                    <button className="border border-black flex items-center gap-2 text-black hover:font-bold font-medium 2xl:py-4 py-2 px-2  2xl:px-7 rounded-xl shadow-md shadow-[#00000025]">
+                      See demo
+                      <ArrowRight className="text-black" />
+                    </button>
+                  </div>
+
+                  <div
+                    data-aos="fade-right"
+                    data-aos-duration="1500"
+                    className="flex  flex-wrap gap-4"
+                  >
+                    <button className="flex items-center gap-2   text-primary-green rounded-xl">
+                      <Image
+                        className="w-full  h-full"
+                        src="/play2.png"
+                        alt="Apple"
+                        width={180}
+                        height={400}
+                      />
+                    </button>
+                    <button className="flex items-center gap-2   text-primary-green rounded-xl">
+                      <Image
+                        className="w-full  h-full "
+                        src="/apple2.png"
+                        alt="Apple"
+                        width={180}
+                        height={400}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="2xl:flex xl:flex lg:flex md:flex hidden items-end relative w-full  justify-end ">
+              <div
+                data-aos="fade-left"
+                data-aos-duration="1500"
+                className="w-full relative z-0 "
+              >
+                <Image
+                  className="xl:w-[1500px] w-full 2xl:w-[1500px]  h-full"
+                  src="/solutions/herorevops.svg"
+                  alt="Center Image"
+                  width={842}
+                  height={463}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className=" mx-auto w-full flex items-center justify-center 2xl:mb-20 py-10 2xl:py-20 ">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1800"
+          data-aos-delay="600"
+          className="flex flex-col items-center justify-center gap-y-10"
+        >
+          <div>
+            <LoadingBar />
+          </div>
+        </div>
+      </section>
+      <section className=" p-20">
+        <div className="relative mx-auto rounded-t-[40px] items-center justify-center py-10 2xl:py-20 bg-[#E2F0CB4D]/30">
+          <div className="flex flex-col items-center justify-center mx-auto gap-2">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1200"
+              data-aos-offset="200"
+              className="bg-[#03473714] py-2 px-3.5 flex items-center gap-3 rounded-full text-[12px] font-semibold uppercase max-w-fit"
+            >
+              maintain
+            </div>
+
+            <h1
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              data-aos-delay="300"
+              className="text-center text-[22px] 2xl:text-[42px] leading-normal"
+            >
+              <span className="text-black font-extrabold">
+                Maintain data quality{" "}
+              </span>
+              <span className="text-black font-extralight">
+                {" "}
+                and management
+              </span>
+            </h1>
+
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1800"
+              data-aos-delay="600"
+              className="items-center justify-center mx-auto relative 2xl:right-12 "
+            >
+              <GridComponent />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="relative w-full 2xl:mt-20">
+        <div className="2xl:flex xl:flex hidden flex-col  2xl:max-w-[1921px] w-full 2xl:max-h-[973px] h-full justify-center mx-auto   items-center gap-4  ">
+          <ImageGalleryLine />
+        </div>
+        <div className="2xl:hidden xl:hidden flex flex-col  2xl:max-w-[1921px] w-full 2xl:max-h-[973px] h-full justify-center mx-auto   items-center gap-4  ">
+          <ImageGalleryLineResponsive />
+        </div>
+      </section>
+
+      <section className="relative mx-auto w-full items-center justify-center xl:mt-0 mt-10 2xl:mt-0 py-10 bg-[#FAFBFC]">
+        <div className="flex flex-col items-center justify-center mx-auto gap-2">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1200"
+            data-aos-offset="200"
+            className="bg-[#03473714] py-2 px-3.5 flex items-center gap-3 rounded-full text-[12px] font-semibold uppercase max-w-fit"
+          >
+            analytics
+          </div>
+
+          <h1
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-delay="300"
+            className="text-center text-[32px] 2xl:text-[42px] leading-normal"
+          >
+            <span className="text-black font-bold">Enhance</span>
+            <span className="text-black font-extralight">
+              {" "}
+              reporting and analytics
+            </span>
+          </h1>
+
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1800"
+            data-aos-delay="600"
+            className="items-center justify-center mx-auto relative 2xl:right-12 "
+          >
+            <GridComponentSecond />
+          </div>
+        </div>
+      </section>
+      <section className="mx-auto w-full flex items-center justify-center py-10 2xl:py-40 ">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="1800"
+          data-aos-delay="600"
+          className="flex flex-col items-center justify-center gap-y-10"
+        >
+          <div>
+            <LoadingBarSecond />
+          </div>
+        </div>
+      </section>
+
+      <section className=" overflow-hidden bg-[#E2F0CB]  ">
+        <div className="items-center justify-center flex flex-col gap-y-4  overflow-hidden ">
+          <Image
+            src="/desing.png"
+            width={1200}
+            height={1000}
+            className="w-full  xl:flex  absolute h-[1000px] 2xl:flex hidden  transform scale-y-[-1]  translate-x-10  z-0 translate-y-[500px] "
+            alt="image"
+          />
+        </div>
+
+        <MarketingStreamline />
+      </section>
+      <Footer />
+    </main>
+  );
+};
+
+export default page;
