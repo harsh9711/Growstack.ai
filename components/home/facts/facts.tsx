2d04340426d00b07f3b038db27d3cdbad4823500
aman-wb <aman@webbuddy.agency>
2024-09-16 14:51:27 +0530

    Fix : mobile modal fixes

--- components/home/facts/facts.tsx
+++ components/home/facts/facts.tsx
@@ -17,33 +17,38 @@ function Facts() {
                         <div className='col-md-6 flex flex-col gap-y-2' data-aos='fade-up' data-aos-easing='ease-in-sine' data-aos-duration='1000'>
                             <span className='user w-fit my-3'>GrowStack Facts</span>
 
-                            <h2 className='text-[38px]'>
-                                <span className="font-bold">Driven by Expertise,</span> <br /> <span className="font-extralight"> Refined by Feedback</span>
-                            </h2>
+                            <p className="header text-left text-[24px] sm:text-[38px]">
+                                <span className=" font-bold">Driven by Expertise,</span>
+                                <br />
+                                <span>
+                                    Refined by Feedback
+                                </span>
+                            </p>
 
-                            <Link href="/auth/register" className='sheen getStart'>
+
+                            <Link href="/company" className='sheen getStart'>
                                 More About Us
                                 <RightArrow color="#034737" />
                             </Link>
                         </div>
 
                         <div className='col-md-6' data-aos='fade-up' data-aos-easing='ease-in-sine' data-aos-duration='1000'>
-                            <div className="grid grid-cols-2 gap-4">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div className="stats">
                                     <h3>600+</h3>
                                     <span>Hours of development</span>
                                 </div>
                                 <div className="stats">
-                                    <h3>600+</h3>
-                                    <span>Hours of development</span>
+                                    <h3>50+</h3>
+                                    <span>User feedback sessions</span>
                                 </div>
                                 <div className="stats">
-                                    <h3>600+</h3>
-                                    <span>Hours of development</span>
+                                    <h3>20+</h3>
+                                    <span>Key features designed</span>
                                 </div>
                                 <div className="stats">
-                                    <h3>600+</h3>
-                                    <span>Hours of development</span>
+                                    <h3>20+</h3>
+                                    <span>Years of industry experience</span>
                                 </div>
                             </div>
                         </div>
