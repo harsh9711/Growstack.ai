301fc9ac9c35f2fdb4976a64bdf6504d8c0b5b07
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-09-26 01:05:07 -0700

    mobile responsive

--- app/solutions/largeenterprise/components/marketingStreamline/MarketingStreamline.tsx
+++ app/solutions/largeenterprise/components/marketingStreamline/MarketingStreamline.tsx
@@ -26,7 +26,7 @@ const MarketingStreamline: React.FC = () => {
             <span className="user">Streamline</span>
             <h3 className="text-black gap-2 mt-4 flex flex-col leading-10  font-light text-[26px] 2xl:text-[40px]">
               {" "}
-              <span className="font-extrabold whitespace-nowrap">Streamline your entire marketing process</span>
+              <span className="font-extrabold xl:whitespace-nowrap">Streamline your entire marketing process</span>
               from to execution and beyond
             </h3>
           </div>
