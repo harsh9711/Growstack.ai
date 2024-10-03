a445728ce2bcc037165b3886d25251b08110977a
aman-wb <aman@webbuddy.agency>
2024-10-03 18:08:04 +0530

    Fix : show chat icon in all the app pages

--- providers/IntercomProvider.tsx
+++ providers/IntercomProvider.tsx
@@ -28,35 +28,30 @@ export default function IntercomProvider({ children }: { children: React.ReactNo
     };
   }, []);
 
-  const isAppRoute = pathname === "/app";
 
   return (
     <Fragment>
       {children}
-      {
-        isAppRoute && (
-          <div
-            typeof="button"
-            id="intercom-launcher"
-            className=" rounded-full  fixed cursor-pointer bottom-10 right-10 z-50"
-          >
-            <div className="relative ">
-              <Image src="/assets/chat.png" alt="Chat icon" width={40} height={40} />
-              {unreadCount > 0 && (
-                <span
-                  style={{
-                    right: "-0.6rem",
-                    top: "-0.4rem",
-                  }}
-                  className="absolute top-0  bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center"
-                >
-                  {unreadCount}
-                </span>
-              )}
-            </div>
-          </div>
-        )
-      }
+      <div
+        typeof="button"
+        id="intercom-launcher"
+        className=" rounded-full  fixed cursor-pointer bottom-10 right-10 z-50"
+      >
+        <div className="relative ">
+          <Image src="/assets/chat.png" alt="Chat icon" width={40} height={40} />
+          {unreadCount > 0 && (
+            <span
+              style={{
+                right: "-0.6rem",
+                top: "-0.4rem",
+              }}
+              className="absolute top-0  bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center"
+            >
+              {unreadCount}
+            </span>
+          )}
+        </div>
+      </div>
     </Fragment>
   );
 }
