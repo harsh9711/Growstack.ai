26d7a7d4f0f31c99fa69d62bfb87ba519c09d4cf
aman-wb <aman@webbuddy.agency>
2024-10-07 15:10:51 +0530

    chore : Remove sonnet and opus model from ai templates

--- app/(main-app)/app/plan/ai-templates/[appId]/page.tsx
+++ app/(main-app)/app/plan/ai-templates/[appId]/page.tsx
@@ -772,8 +772,6 @@ export default function AiAppPage({
                 "gpt-3.5-turbo",
                 "gpt-4",
                 "gpt-4o",
-                "claude-3-5-sonnet-20240620",
-                "claude-3-opus-20240229",
                 "claude-3-sonnet-20240229",
                 "claude-3-haiku-20240307",
                 "gemini-1.5-flash",
