d0612e005e48fd697f047b7bff8320bb1ed9b85e
harshWebbuddy <165643599+harshWebbuddy@users.noreply.github.com>
2024-10-08 16:32:09 +0530

    Landingpagefix (#220)
    * landingpage fixing
    
    * changes
    
    * landingpage fix
    
    * landingpage fixed
    
    * fixed

--- components/home/faq/Faqs.js
+++ components/home/faq/Faqs.js
@@ -45,19 +45,19 @@ const CustomAccordion = () => {
         const isActive = activeKey === index;
         
         return (
-          <div key={index} className="mb-4 w-full   bg-white border text-[20px] rounded-[20px] font-bold items-start ease-in-out transition-transform duration-1000 cursor-pointer">
+          <div key={index} className="mb-4 w-full   bg-white border text-[12px] sm:text-[20px] rounded-[20px] font-bold items-start ease-in-out transition-transform duration-1000 cursor-pointer">
             <div
               className={`flex flex-row w-full items-center px-8 py-4 justify-between cursor-pointer`}
               onClick={() => handleToggle(index)}
             >
               <div
-                className={`rounded-[20px] text-start flex flex-row leading-8 items-center w-full justify-between ${isActive ? "text-primary-green" : ""}`}
+                className={`rounded-[20px] text-start flex flex-row leading-tight items-center w-full justify-between ${isActive ? "text-primary-green" : ""}`}
               >
                 {item.header}
               </div>
 
               <div
-                className={`flex ease-in-out transition-transform duration-300 items-center justify-center border w-12 h-12 bg-${isActive ? "primary-green" : "white"} rounded-full ${isActive ? "-rotate-180" : "rotate-75"}`}
+                className={`sm:flex hidden ease-in-out transition-transform duration-300 items-center justify-center sm:border sm:w-12 sm:h-12 bg-${isActive ? "primary-green" : "white"} rounded-full ${isActive ? "-rotate-180" : "rotate-75"}`}
               >
                 <svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M1.85156 1.93066L10.0003 10.0426L18.1491 1.93066" stroke={isActive ? "white" : "#034737"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -69,7 +69,7 @@ const CustomAccordion = () => {
               className={`overflow-hidden transition-all duration-700 ease-linear ${isActive ? "max-h-40" : "max-h-0"}`}
              
             >
-              <div className="rounded-[20px] border-b-8 border-[#D9D9D9] p-4  w-full font-normal text-[18px] text-gray-700 bg-white">
+              <div className="rounded-[20px] border-b-8 border-[#D9D9D9] p-4  w-full font-normal text-[12px] sm:text-[18px] text-gray-700 bg-white">
                 {item.body}
               </div>
             </div>
