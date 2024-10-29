56188c1906e70ee07c82cd47e609ff27b8eeb704
Mukesh Arya <Mukhesh.Kasimahanthi@bilvantis.io>
2024-10-29 10:15:21 +0530

    worked on custom gpt

--- app/(main-app)/app/ai-studio/custom-gpts/page.tsx
+++ app/(main-app)/app/ai-studio/custom-gpts/page.tsx
@@ -250,10 +250,10 @@ export default function Customgpts() {
                             <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-10">
                               <ul className="py-1">
                                 <li className="px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => { CustomGptPublic(_id,true) }}>
-                                set as public
+                                Set as public
                                 </li>
                                 <li className="px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => { CustomGptPublic(_id,false) }}>
-                                remove from public
+                                Remove from public
                                 </li>
                                 <li className="px-4 py-2 cursor-pointer text-rose-600 hover:bg-gray-100" onClick={() => { setIsDeleting(true);setDeleteId(_id) }}>
                                   Delete
