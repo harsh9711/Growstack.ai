777419f5d062e9f95ba991f59133a6c17c0f1ea7
Mukesh Arya <Mukhesh.Kasimahanthi@bilvantis.io>
2024-10-30 11:59:07 +0530

    worked on custom gpt

--- app/(main-app)/app/ai-studio/custom-gpts/page.tsx
+++ app/(main-app)/app/ai-studio/custom-gpts/page.tsx
@@ -27,18 +27,18 @@ export default function Customgpts() {
   const [customGptsUser, setCustomGptsUser] = useState<CustomGpt[]>([]);
   const [menuOpenIndex, setMenuOpenIndex] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [isDeleting,setIsDeleting] = useState(false)
-  const [deleteId,setDeleteId] = useState("")
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [deleteId, setDeleteId] = useState("")
   const [viewAll, setViewAll] = useState(false);
   const getCustomGpts = async () => {
     setLoading(true);
     try {
       const { data: { data, publicdata } } = await instance.get(`${API_URL}/ai/api/v1/customgpt`);
-      publicdata? setCustomGpts(publicdata
-          .filter((d: any) => d.is_public === true)
-          .map((d: any) => ({ ...d, show: true }))
-      ):""
-      setCustomGptsUser(data.map((d: any) => ({ ...d, show: true })))
+      publicdata ? setCustomGpts(publicdata
+        .filter((d: any) => d.is_public === true)
+        .map((d: any) => ({ ...d, show: true }))
+      ) : ""
+      data ? setCustomGptsUser(data.filter((d: any) => d.is_delete === false).map((d: any) => ({ ...d, show: true }))) : ""
     } catch (error: any) {
       if (error.response) {
         toast.error(error.response.data.error);
@@ -49,10 +49,10 @@ export default function Customgpts() {
       setLoading(false);
     }
   };
-  const CustomGptPublic = async (id: string,makePublic:boolean) => {
+  const CustomGptPublic = async (id: string, makePublic: boolean) => {
     setLoading(true);
     try {
-      await instance.post(`${API_URL}/ai/api/v1/customgpt/changetopublicaccess`, { "_id": id,"makePublic":makePublic });
+      await instance.post(`${API_URL}/ai/api/v1/customgpt/changeToPublicAccess`, { "_id": id, "makePublic": makePublic });
       setMenuOpenIndex(null)
       getCustomGpts()
     }
@@ -71,7 +71,7 @@ export default function Customgpts() {
     setLoading(true);
     setMenuOpenIndex(null)
     try {
-      await instance.post(`${API_URL}/ai/api/v1/customgpt/deletecustomgpt`, { "_id": deleteId });
+      await instance.post(`${API_URL}/ai/api/v1/customgpt/deleteCustomGpt`, { "_id": deleteId });
       setIsDeleting(false)
       setMenuOpenIndex(null)
       getCustomGpts()
@@ -131,7 +131,7 @@ export default function Customgpts() {
             Array(7)
               .fill(null)
               .map((_, index) => <SkeletonLoader key={index} />)
-          ) : customGpts.length < 1 && customGptsUser.length<1 ? (
+          ) : customGpts.length < 1 && customGptsUser.length < 1 ? (
             <div className="mt-4 hover:scale-120 flex flex-col justify-center items-center space-y-4 col-span-3 py-8">
               <h2 className="text-lg text-center font-semibold">
                 You have Custom GPTs Created yet
@@ -150,60 +150,60 @@ export default function Customgpts() {
             <>
               <h1 className="text-2xl font-semibold">GPTs by GrowStack</h1>
               <div className="grid grid-cols-3 gap-5 mt-8">
-              {visibleItems.map(({ description, icon, name, _id, show }, index) =>
-        show && (
-          <div
-            key={_id}
-            className="bg-white border border-[#E8E8E8] rounded-2xl p-6 hover:shadow-2xl hover:shadow-gray-300 cursor-pointer transition-all duration-300 relative"
-          >
-            <div className="flex justify-between">
-              <Link href={`/app/ai-studio/custom-gpts/gpt?custom_gpt_id=${_id}`}>
-                <div className="flex items-center gap-5">
-                  <Image
-                    src={icon}
-                    alt=""
-                    width={200}
-                    height={200}
-                    className="rounded-2xl w-[90px] h-[90px] object-cover"
-                  />
-                  <div className="space-y-2">
-                    <h1 className="text-lg font-semibold">{name}</h1>
-                    <p
-                      className="text-primary-black text-opacity-50 leading-relaxed"
-                      style={{
-                        display: "-webkit-box",
-                        WebkitLineClamp: 3,
-                        WebkitBoxOrient: "vertical",
-                        overflow: "hidden",
-                        width: "80%",
-                      }}
+                {visibleItems.map(({ description, icon, name, _id, show }, index) =>
+                  show && (
+                    <div
+                      key={_id}
+                      className="bg-white border border-[#E8E8E8] rounded-2xl p-6 hover:shadow-2xl hover:shadow-gray-300 cursor-pointer transition-all duration-300 relative"
                     >
-                      {description}...
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </div>
-        )
-      )}
+                      <div className="flex justify-between">
+                        <Link href={`/app/ai-studio/custom-gpts/gpt?custom_gpt_id=${_id}`}>
+                          <div className="flex items-center gap-5">
+                            <Image
+                              src={icon}
+                              alt=""
+                              width={200}
+                              height={200}
+                              className="rounded-2xl w-[90px] h-[90px] object-cover"
+                            />
+                            <div className="space-y-2">
+                              <h1 className="text-lg font-semibold">{name}</h1>
+                              <p
+                                className="text-primary-black text-opacity-50 leading-relaxed"
+                                style={{
+                                  display: "-webkit-box",
+                                  WebkitLineClamp: 3,
+                                  WebkitBoxOrient: "vertical",
+                                  overflow: "hidden",
+                                  width: "80%",
+                                }}
+                              >
+                                {description}...
+                              </p>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                  )
+                )}
 
-      {customGpts.length > 5 && !viewAll && (
-        <button
-          onClick={() => setViewAll(true)}
-          className="mt-4 w-20px h-10px px-4 py-2 rounded text-green-500"
-        >
-          View All
-        </button>
-      )}
-      {customGpts.length > 5 && viewAll && (
-        <button
-          onClick={() => setViewAll(false)}
-          className="mt-4 w-20px h-10px px-4 py-2 rounded text-green-500"
-        >
-          Minimize
-        </button>
-      )}
+                {customGpts && customGpts.length > 5 && !viewAll && (
+                  <button
+                    onClick={() => setViewAll(true)}
+                    className="mt-4 w-20px h-10px px-4 py-2 rounded text-green-500"
+                  >
+                    View All
+                  </button>
+                )}
+                {/* {customGpts && customGpts.length > 5 && viewAll && (
+                  <button
+                    onClick={() => setViewAll(false)}
+                    className="mt-4 w-20px h-10px px-4 py-2 rounded text-green-500"
+                  >
+                    Minimize
+                  </button>
+                )} */}
               </div>
               {customGpts?.length == 0 && <h1 className="text-center">GPTs by GrowStack Records Not Available</h1>}
               <div className="mt-8 mb-3">
@@ -211,7 +211,7 @@ export default function Customgpts() {
               </div>
               <h1 className="text-2xl font-semibold mt-4">GPTs Created by users</h1>
               <div className="grid grid-cols-3 gap-5 mt-8">
-                {customGptsUser.map(({ description, icon, name, _id, show, pre_built, is_public }, index) =>
+                {customGptsUser && customGptsUser.map(({ description, icon, name, _id, show, pre_built, is_public }, index) =>
                   show && (
                     <div className="bg-white border border-[#E8E8E8] rounded-2xl p-6 hover:shadow-2xl hover:shadow-gray-300 cursor-pointer transition-all duration-300 relative">
                       <div className="flex justify-between">
@@ -249,13 +249,13 @@ export default function Customgpts() {
                           {menuOpenIndex === index && (
                             <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-10">
                               <ul className="py-1">
-                                <li className="px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => { CustomGptPublic(_id,true) }}>
-                                Set as public
+                                <li className="px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => { CustomGptPublic(_id, true) }}>
+                                  Set as public
                                 </li>
-                                <li className="px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => { CustomGptPublic(_id,false) }}>
-                                Remove from public
+                                <li className="px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => { CustomGptPublic(_id, false) }}>
+                                  Remove from public
                                 </li>
-                                <li className="px-4 py-2 cursor-pointer text-rose-600 hover:bg-gray-100" onClick={() => { setIsDeleting(true);setDeleteId(_id) }}>
+                                <li className="px-4 py-2 cursor-pointer text-rose-600 hover:bg-gray-100" onClick={() => { setIsDeleting(true); setDeleteId(_id) }}>
                                   Delete
                                 </li>
                               </ul>
@@ -293,37 +293,37 @@ export default function Customgpts() {
           )}
         </div>
         <GlobalModal
-        className=""
-        open={isDeleting}
-        setOpen={() => {
-          setIsDeleting(false);
-        }}
-      >
-        <div className="flex flex-col items-start justify-center px-6 pt-4 pb-8 gap-6 ">
-          <h3 className="text-center text-xl font-semibold">
-            Are you sure you want to delete{" "}
-            <strong>GPT</strong>
-          </h3>
-          <p className="text-left text-gray-700 text-sm md:text-base">
-            This action is irreversible. Once deleted, you will not be able to
-            recover this GPT.
-          </p>
-          <div className="flex items-center justify-start gap-3">
-            <button
-              className="text-gray-500 border border-gray-500 bg-transparent text-nowrap py-2 px-8 rounded-md transition duration-300"
-              
-            >
-              Cancel
-            </button>
-            <button onClick={()=>{deleteGpt()}}
-              className="text-white bg-red-500 text-nowrap flex items-center gap-1 py-2 px-6 rounded-md transition duration-300 hover:bg-red-600"
-            >
-             <Trash2 size={18} />
-              Delete
-            </button>
+          className=""
+          open={isDeleting}
+          setOpen={() => {
+            setIsDeleting(false);
+          }}
+        >
+          <div className="flex flex-col items-start justify-center px-6 pt-4 pb-8 gap-6 ">
+            <h3 className="text-center text-xl font-semibold">
+              Are you sure you want to delete{" "}
+              <strong>GPT</strong>
+            </h3>
+            <p className="text-left text-gray-700 text-sm md:text-base">
+              This action is irreversible. Once deleted, you will not be able to
+              recover this GPT.
+            </p>
+            <div className="flex items-center justify-start gap-3">
+              <button
+                className="text-gray-500 border border-gray-500 bg-transparent text-nowrap py-2 px-8 rounded-md transition duration-300"
+
+              >
+                Cancel
+              </button>
+              <button onClick={() => { deleteGpt() }}
+                className="text-white bg-red-500 text-nowrap flex items-center gap-1 py-2 px-6 rounded-md transition duration-300 hover:bg-red-600"
+              >
+                <Trash2 size={18} />
+                Delete
+              </button>
+            </div>
           </div>
-        </div>
-      </GlobalModal>
+        </GlobalModal>
       </main>
     </Fragment>
   );
