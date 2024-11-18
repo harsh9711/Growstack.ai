2d2b7b63a8227b177a9e0dcedef58bef0efe0e7e
puneetyuvitech <puneetyuvitech@gmail.com>
2024-11-18 10:55:01 -0600

    Added the new nodes

--- app/workflow/nodes/types.ts
+++ app/workflow/nodes/types.ts
@@ -6,4 +6,33 @@ export type ChatGptNodeProps = Node<
   { label: string; image: ImageProps },
   "chat-gpt"
 >;
-export type AppNode = BuiltInNode | PositionLoggerNode | ChatGptNodeProps;
+export type ShortTexttNodeProps = Node<
+  { label: string; image: ImageProps },
+  "short-text"
+>;
+
+export type LongTexttNodeProps = Node<
+  { label: string; image: ImageProps },
+  "long-text"
+>;
+
+export type BooleanNodeProps = Node<
+  { label: string; image: ImageProps },
+  "boolean"
+>;
+export type NumberNodeProps = Node<
+  { label: string; image: ImageProps },
+  "number"
+>;
+export type FileUploadNodeProps = Node<
+  { label: string; image: ImageProps },
+  "fileUpload"
+>;
+export type AppNode =
+  | BuiltInNode
+  | ChatGptNodeProps
+  | ShortTexttNodeProps
+  | LongTexttNodeProps
+  | BooleanNodeProps
+  | NumberNodeProps
+  | FileUploadNodeProps;
