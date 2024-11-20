7e59b607236f905471e42d834ca8583cd324c6f3
balaramakrishna <balaramakrishna.kamma@bilvantis.io>
2024-11-20 11:46:40 +0530

    changed layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,15 +39,15 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-CLCETMEDBL"
         ></Script>
-        <Script id="google-analytics">
+        {/* <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-CLCETMEDBL');
           `}
-        </Script>
-        <Script id="happierleads" strategy="lazyOnload">
+        </Script> */}
+        {/* <Script id="happierleads" strategy="lazyOnload">
           {`
             !function(){var e="rest.happierleads.com/v3/script?clientId=ehacgbpBj346Npwip2yrSJ&version=4.0.0",
             t=document.createElement("script");window.location.protocol.split(":")[0];
@@ -56,7 +56,7 @@ export default function RootLayout({
             t.onload = function(){ new Happierleads.default };
             c.parentNode.insertBefore(t,c)}();
           `}
-        </Script>
+        </Script> */}
         <Script
           src="https://app.10xlaunch.ai/widget"
           data-app-id="889a8e5c-0c2c-44cb-8494-a1978142001e"
