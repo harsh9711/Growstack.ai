ba4d5c24bb35c5e2e8b8da4a6414acede8604642
jay <jay.salot@bitontree.com>
2024-12-31 10:33:49 +0530

    fix: copy change v1

--- app/(main-app)/app/workflow/[workflow_id]/components/nodes/GeneralInputNodes.tsx
+++ app/(main-app)/app/workflow/[workflow_id]/components/nodes/GeneralInputNodes.tsx
@@ -383,7 +383,7 @@ const GeneralInputNodes = memo(
                           <div className="loader ease-linear rounded-full border-4 border-gray-200 border-t-4 border-t-[#2DA771] h-6 w-6" />
                         </div>
                       ) : (
-                        "Next"
+                        "Save"
                       )}
                     </button>
                   </div>
