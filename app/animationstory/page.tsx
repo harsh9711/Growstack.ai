683b41c1ac6bdf0a262cf15ff90146684f9a54a5
balaramakrishna <balaramakrishna.kamma@bilvantis.io>
2025-01-06 15:08:03 +0530

    added video in mobile view

--- app/animationstory/page.tsx
+++ app/animationstory/page.tsx
@@ -2,17 +2,14 @@
 import React, { useRef, useState, useEffect } from "react";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import dynamic from "next/dynamic";
 
-// Lazy load the KeyPoints component
-const KeyPoints = dynamic(() => import("./KeyPoints"), {
-  ssr: false,
-});
 const AnimationStory = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const mainVideoRef = useRef<HTMLVideoElement | null>(null);
+  const mobileVideoRef = useRef<HTMLVideoElement | null>(null);
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
   useEffect(() => {
-    const videoElement = videoRef.current;
+    const videoElement = mainVideoRef.current;
     const handleIntersection = (entries: IntersectionObserverEntry[]) => {
       const [entry] = entries;
       setIsVideoLoaded(entry.isIntersecting);
@@ -42,23 +39,24 @@ const AnimationStory = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-screen max-h-[550px] sm:max-h-[1000px] border-none outline-none overflow-y-hidden bg-[#F5F5F5] text-white">
+    <div className="relative w-full md:h-screen max-h-[550px] sm:max-h-[1000px] border-none outline-none overflow-y-hidden bg-[#F5F5F5] text-white">
+      {/* Main video for tablet and larger screens */}
       <video
-        ref={videoRef}
+        ref={mainVideoRef}
         src="https://growstack-static-content.s3.us-east-1.amazonaws.com/landingpagevideos.mp4"
         autoPlay
         playsInline
         loop
         muted
         preload="auto"
-        className={`absolute top-0 left-0 w-full h-full ease-out hidden sm:block border-none outline-none right-0 transition-opacity duration-700 ${
+        className={`absolute top-0 left-0 w-full h-full ease-out hidden sm:block transition-opacity duration-700 ${
           isVideoLoaded ? "opacity-100" : "opacity-0"
         }`}
       >
         Your browser does not support the video tag.
       </video>
 
-      <div className="relative z-10 mx-auto flex flex-col sm:mt-2 max-w-[1320px] p-4">
+      <div className="relative z-10 mx-auto flex flex-col items-center justify-center sm:mt-2 max-w-[1320px] p-4">
         <div className="flex w-full flex-col gap-6 sm:flex-row items-center justify-between">
           <h1 className="text-black text-[16px] sm:text-[24px] md:text-[36px] lg:text-[42px] text-center sm:text-start max-w-[620px] w-full font-medium mb-1">
             AI for
@@ -67,13 +65,13 @@ const AnimationStory = () => {
             </span>
           </h1>
           <div className="flex flex-col text-center sm:text-start sm:text-[16px] md:text-[20px] font-semibold">
-            <h2 className="text-[#2DA771] text-start">
+            <h2 className="text-[#2DA771] text-start ml-1">
               No More <span className="text-black">Silos</span>
             </h2>
-            <h2 className="text-[#2DA771] text-start">
+            <h2 className="text-[#2DA771] text-start ml-1">
               No More <span className="text-black">Delays</span>
             </h2>
-            <h2 className="text-[#2DA771] text-start">
+            <h2 className="text-[#2DA771] text-start ml-1">
               No More <span className="text-black">Limits</span>
             </h2>
             <div className="mt-2">
@@ -86,9 +84,20 @@ const AnimationStory = () => {
           </div>
         </div>
 
-        {/* Key Points Section */}
-        <div className="flex sm:hidden w-full mx-auto items-center justify-center max-w-[1320px] mt-6 sm:mt-10">
-          <KeyPoints />
+        {/* Mobile video section */}
+        <div className="block sm:hidden w-full mx-auto items-center justify-center max-w-[1320px] mt-6">
+          <video
+            ref={mobileVideoRef}
+            src="https://growstack-static-content.s3.us-east-1.amazonaws.com/landingpagevideos.mp4"
+            autoPlay
+            playsInline
+            loop
+            muted
+            preload="auto"
+            className="w-full h-full rounded-lg"
+          >
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </div>
